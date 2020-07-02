0d4263bb6d18327e34e1cf9efaadac9faaa4d187
Pratikkshirsagar <meetpratiik@gmail.com>
2020-07-02 18:40:46 +0530

    search jobs from distance api added

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,4 +1,5 @@
 const Job = require('../models/jobs');
+const geoCoder = require('../utils/geocoder');
 
 // Get all jobs => /api/v1/jobs
 exports.getJobs = async (req, res, next) => {
@@ -26,5 +27,33 @@ exports.createJob = async (req, res, next) => {
     });
   } catch (error) {
     console.log(error);
+  }
+};
+
+// Search job within radius => /api/v1/jobs/:zipcode/:distance
+exports.getJobsInRadius = async (req, res, next) => {
+  try {
+    const { zipcode, distance } = req.params;
+
+    // getting latitude and longitude from geocoder with zipcode
+    const loc = await geoCoder.geocode(zipcode);
+    const latitude = loc[0].latitude;
+    const longitude = loc[0].longitude;
+
+    const radius = distance / 3963;
+
+    const jobs = await Job.find({
+      location: {
+        $geoWithin: { $centerSphere: [[longitude, latitude], radius] },
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      results: jobs.length,
+      data: jobs,
+    });
+  } catch (err) {
+    console.log(err);
   }
 };
--- routes/jobs.js
+++ routes/jobs.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 // Importing job controller method
-const { getJobs, createJob } = require('../controllers/jobsController');
+const {
+  getJobs,
+  createJob,
+  getJobsInRadius,
+} = require('../controllers/jobsController');
 
 router.route('/jobs').get(getJobs).post(createJob);
+
+router.route('/jobs/:zipcode/:distance').get(getJobsInRadius);
 
 module.exports = router;
