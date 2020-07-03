59c3a0a178c003303bd7f550cc13359c39bdf7ab
Pratikkshirsagar <meetpratiik@gmail.com>
2020-07-03 17:21:28 +0530

    handle unhandle routes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const dotenv = require('dotenv');
 
 const conectDatabase = require('./config/db');
 const errorMiddleware = require('./middleware/errors');
+const ErrorHandler = require('./utils/errorHandler');
+
 // Setting up config.env variables
 dotenv.config({ path: './config/config.env' });
 
@@ -21,19 +23,15 @@ conectDatabase();
 // set up bodyparser
 app.use(express.json());
 
-const middelware = (req, res, next) => {
-  console.log('Hello from middleware');
-  req.user = 'Pratik Kshirsagar';
-  next();
-};
-
-app.use(middelware);
-
 // Importing all Routes
 const jobs = require('./routes/jobs');
 
 app.use('/api/v1', jobs);
 
+// Handle unhandle routes
+app.all('*', (req, res, next) => {
+  next(new ErrorHandler(`${req.originalUrl} route not found`, 404));
+});
 // middleware to handle errors
 app.use(errorMiddleware);
 
