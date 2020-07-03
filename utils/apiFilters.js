4e8470618004a19b5912654e48b0479beb7834fa
Pratikkshirsagar <meetpratiik@gmail.com>
2020-07-04 00:26:37 +0530

    limiting fields and pagination added.

--- utils/apiFilters.js
+++ utils/apiFilters.js
@@ -7,8 +7,11 @@ class APIFilters {
   filter() {
     const queryCopy = { ...this.queryStr };
 
-    // Advance filter using: lt, lte, gt, gte
+    // Removing fields from the query
+    const removeFields = ['sort', 'fields', 'limit', 'page'];
+    removeFields.forEach((el) => delete queryCopy[el]);
 
+    // Advance filter using: lt, lte, gt, gte
     let queryStr = JSON.stringify(queryCopy);
     queryStr = queryStr.replace(
       /\b(gt|gte|lt|lte|in)\b/g,
@@ -16,6 +19,35 @@ class APIFilters {
     );
 
     this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  sort() {
+    if (this.queryStr.sort) {
+      const sortBy = this.queryStr.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-postingDate');
+    }
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryStr.fields) {
+      const fields = this.queryStr.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+    return this;
+  }
+
+  pagination() {
+    const page = parseInt(this.queryStr.page, 10) || 1;
+    const limit = parseInt(this.queryStr.limit, 10) || 10;
+    const skipResults = (page - 1) * limit;
+
+    this.query = this.query.skip(skipResults).limit(limit);
     return this;
   }
 }
